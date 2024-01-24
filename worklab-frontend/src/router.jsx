c7af5ce5d299507fecbfa62293f9fb2d7fcc8a16
Renato Oliveira <renato.printf@gmail.com>
2024-01-24 13:04:31 -0300

    new feature: barra de navegaçao

--- worklab-frontend/src/router.jsx
+++ worklab-frontend/src/router.jsx
@@ -3,20 +3,36 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import CadastrarPacientes from './pages/CadastrarPacientes'
 import CadastrarExames from './pages/CadastrarExames'
+import DefaultLayout from './components/DefaultLayout'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />
   },
+  // {
+  //   path: '/cadastrar/paciente',
+  //   element: <CadastrarPacientes />
+  // },
+  // {
+  //   path: '/cadastrar/exame',
+  //   element: <CadastrarExames />
+  // },
   {
-    path: '/cadastrar/paciente',
-    element: <CadastrarPacientes />
-  },
-  {
-    path: '/cadastrar/exame',
-    element: <CadastrarExames />
+    path: '/cadastrar',
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: 'paciente',
+        element: <CadastrarPacientes />
+      },
+      {
+        path: 'exame',
+        element: <CadastrarExames />
+      },
+    ]
   }
+  
 ])
 
 export default router
