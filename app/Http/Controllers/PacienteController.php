5ddbd5ff9a70f8fa66df385fc15deaa682c07279
Renato Oliveira <renato.printf@gmail.com>
2024-01-25 14:20:42 -0300

    new feature: implementado metodos nos controladores

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -7,4 +7,34 @@ use Illuminate\Http\Request;
 class PacienteController extends Controller
 {
     //
+    public function index()
+    {
+        $pacientes = Paciente::all();
+        return response()->json($pacientes);
+    }
+    public function store(Request $request)
+    {
+        $paciente = Paciente::create($request->all());
+        return response()->json($paciente, 201);
+    }
+
+    public function show($id)
+    {
+        $paciente = Paciente::findOrFail($id);
+        return response()->json($paciente);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $paciente = Paciente::findOrFail($id);
+        $paciente->update($request->all());
+        return response()->json($paciente, 200);
+    }
+
+    public function destroy($id)
+    {
+        $paciente = Paciente::findOrFail($id);
+        $paciente->delete();
+        return response()->json(null, 204);
+    }
 }
