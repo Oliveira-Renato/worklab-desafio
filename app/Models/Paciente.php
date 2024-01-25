fb172a0ef8de0e90a0f3a4a290a6409421ded91a
Renato Oliveira <renato.printf@gmail.com>
2024-01-25 15:00:58 -0300

    fix: fillable

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Paciente extends Model
 {
-    use HasFactory;
+    protected $fillable = ['nome_completo', 'sexo', 'email', 'celular'];
 
     public function exames()
     {
