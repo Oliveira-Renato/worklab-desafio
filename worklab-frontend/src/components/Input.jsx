f602abfb1c0fcb5376e287f68d048a187fdbfdcd
Renato Oliveira <renato.printf@gmail.com>
2024-01-26 11:23:10 -0300

    fix: Componente Input props

--- worklab-frontend/src/components/Input.jsx
+++ worklab-frontend/src/components/Input.jsx
@@ -1,16 +1,21 @@
-export default function Input({name}) {
+const Input = ({label, value, onChange}) => {
   return (
     <div className="sm:col-span-3">
-      <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">
-        {name}
+      <label htmlFor={label} className="block text-sm font-medium leading-6 text-gray-900">
+        {label}
       </label>
       <div className="mt-2">
         <input
+          id={label}
+          name={label}
+          value={value}
           type="text"
-          name={name.toLowerCase()}
+          onChange={ (e) => onChange(label, e.target.value )}
           className="block w-full rounded-md ring-1 border-gray-300 p-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-400 focus:border-indigo-400 sm:text-sm sm:leading-5 outline-none transition-all duration-300 ease-in-out"
-        />
-      </div>
-  </div>
+          />
+        </div>
+    </div>
   )
 }
+
+export default Input;
