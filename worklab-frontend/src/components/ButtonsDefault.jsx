0ea8c840debfa64d96e3baa7e10cb5db53c73511
Renato Oliveira <renato.printf@gmail.com>
2024-01-29 14:41:45 -0300

    Adding comments: ButtonsDEFAULT.jsx

--- worklab-frontend/src/components/ButtonsDefault.jsx
+++ worklab-frontend/src/components/ButtonsDefault.jsx
@@ -1,11 +1,15 @@
 import { useNavigate } from "react-router-dom";
 
+// Componente funcional ButtonsDefault
 const ButtonsDefault = ({loading}) => {
   const navigateTo = useNavigate()
+
+  // Função para navegar de volta à página inicial
   const handleCancelar = () => navigateTo('/')
 
   return (
     <div className="mt-6 flex items-center justify-end gap-x-6">
+      {/* Botão para cancelar a operação */}
       <button
         type="button"
         onClick={handleCancelar}
@@ -13,6 +17,8 @@ const ButtonsDefault = ({loading}) => {
       >
         Cancelar
       </button>
+
+      {/* Botão para enviar o formulário */}
       <button
         type="submit"
         className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
