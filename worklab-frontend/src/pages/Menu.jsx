7ede82940b2971a13939e285ecc2e18aecba4e7b
Renato Oliveira <renato.printf@gmail.com>
2024-01-29 16:02:23 -0300

    fix: colors menu

--- worklab-frontend/src/pages/Menu.jsx
+++ worklab-frontend/src/pages/Menu.jsx
@@ -4,10 +4,10 @@ import { NavLink, useLocation  } from "react-router-dom";
 
 // Define as opções de navegação
 const navigation = [
-  { name: "Paciente", to: "/pacientes" },
-  { name: "Exame", to: "/exames" },
-  { name: "Vincular Exame", to: "/vincular_exame" },
-  { name: "Relatorio", to: "/relatorio" },
+  { name: "Paciente", to: "/pacientes", color: "#3498DB" },
+  { name: "Exame", to: "/exames", color: "#2ECC71" },
+  { name: "Vincular Exame", to: "/vincular_exame", color: "#f56565"},
+  { name: "Relatorio", to: "/relatorio", color: "#667eea" },
 ]
 
 // Componente Menu
@@ -33,8 +33,7 @@ export default function Menu() {
           <NavLink
             key={item.name}
             to={item.to}
-            className={`text-white font-medium cursor-pointer text-[20px] hover:bg-primary bg-[#1B90C6] w-[160px] h-[160px] rounded-r-sm flex items-center justify-center`
-            }
+            className={`text-white font-medium cursor-pointer text-[20px] hover:bg-primary bg-[${item.color}] w-[160px] h-[160px] rounded-r-sm flex items-center justify-center`}
           >
            {item.name}
           </NavLink>
