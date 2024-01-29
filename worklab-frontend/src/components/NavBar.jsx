74075f9d8c58be80a857c163a3d9ec3dffb8d96e
Renato Oliveira <renato.printf@gmail.com>
2024-01-29 14:36:53 -0300

    Adding comments: NavBar.jsx

--- worklab-frontend/src/components/NavBar.jsx
+++ worklab-frontend/src/components/NavBar.jsx
@@ -3,10 +3,12 @@ import { styles } from "../utils/styles.js";
 import { navLinks } from "../constants/index.js";
 import worklabLog from "../assets/logo-worklab.png";
 
-export default function NavBar() {
+// Componente funcional NavBar
+const NavBar = () => {
   return (
     <nav className={`${styles.paddingX} bg-primary w-full flex items-center`}>
         <div className="w-full flex justify-between items-center text-white  max-w-8xl mx-auto">
+          {/* Link para a página inicial */}
           <Link
             to={'/'}
             className={`flex items-center`}
@@ -18,12 +20,14 @@ export default function NavBar() {
             />
           </Link>
           
+           {/* Lista de links de navegação */}
           <ul className="list-none hidden sm:flex flex-row gap-10">
             {navLinks.map((link) => (
               <li 
                 key={link.id}
                 className={`text-white font-medium cursor-pointer text-[16px] hover:bg-sky-700 p-2 rounded-sm transition-all`}
               >
+                {/* NavLink para cada rota específica */}
                 <NavLink
                   to={link.rota}
                   className={'hover:black'}
@@ -38,3 +42,5 @@ export default function NavBar() {
     </nav>
   )
 }
+
+export default NavBar
