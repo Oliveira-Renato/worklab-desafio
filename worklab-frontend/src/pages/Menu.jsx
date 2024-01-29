44bb01489feb2810351d221f66f8efb1b55f2988
Renato Oliveira <renato.printf@gmail.com>
2024-01-29 14:20:54 -0300

    Adding comments: Menu.jsx

--- worklab-frontend/src/pages/Menu.jsx
+++ worklab-frontend/src/pages/Menu.jsx
@@ -1,6 +1,8 @@
+// Importa React e hooks necessários
 import React from "react";
 import { NavLink, useLocation  } from "react-router-dom";
 
+// Define as opções de navegação
 const navigation = [
   { name: "Paciente", to: "/pacientes" },
   { name: "Exame", to: "/exames" },
@@ -8,19 +10,26 @@ const navigation = [
   { name: "Relatorio", to: "/relatorio" },
 ]
 
+// Componente Menu
 export default function Menu() {
+   // Obtém a localização atual da rota
   const location = useLocation();
+  // Verifica se o menu deve ser ocultado com base na localização atual
   const shouldHideMenu = location.pathname !== "/";
 
+  // Renderiza a estrutura do componente
   return (
     <div className={`${shouldHideMenu ? 'hidden' : ''}`} >
+       {/* Cabeçalho do menu */}
       <div className="w-full flex flex-col items-center bg-gray-200 p-4 text-gray-800 mb-20">
         <h2 className="text-3xl font-bold mb-2">Menu Principal</h2>
         <p className="text-lg">Explore as funcionalidades do nosso sistema.</p>
       </div>
 
+      {/* Opções de navegação */}
       <div className="flex justify-center items-center flex-wrap gap-5">
         {navigation.map((item) => (
+          // Utiliza NavLink para criar links de navegação com estilos ativos
           <NavLink
             key={item.name}
             to={item.to}
