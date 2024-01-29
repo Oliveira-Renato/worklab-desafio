967257e0c69ff50c851f68a6e76df280303bf9e9
Renato Oliveira <renato.printf@gmail.com>
2024-01-29 14:50:14 -0300

    Adding comments: Models/*

--- app/Models/Exame.php
+++ app/Models/Exame.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Exame extends Model
 {
     use HasFactory;
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'id'; // Especifica o nome da chave primária
 
-    protected $fillable = ['codigo', 'descricao', 'valor'];
+    protected $fillable = ['codigo', 'descricao', 'valor']; // Define os campos que podem ser preenchidos em massa
 
+    // Define o relacionamento many-to-many com o modelo Paciente
     public function pacientes()
     {
         return $this->belongsToMany(Paciente::class, 'paciente_exame', 'exame_id', 'paciente_numero_atendimento');
--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Paciente extends Model
 {
     use HasFactory;
-    protected $fillable = ['numero_atendimento', 'nome_completo', 'sexo', 'email', 'celular'];
+    protected $fillable = ['numero_atendimento', 'nome_completo', 'sexo', 'email', 'celular']; // Define os campos que podem ser preenchidos em massa
 
+    // Define o relacionamento many-to-many com o modelo Exame
     public function exames()
     {
         return $this->belongsToMany(Exame::class, 'paciente_exame', 'paciente_numero_atendimento', 'exame_id');
