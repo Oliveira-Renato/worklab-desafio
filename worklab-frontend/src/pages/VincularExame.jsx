4a089d43940c2d6b6b7e233b88788ef71b867dda
Renato Oliveira <renato.printf@gmail.com>
2024-02-02 13:27:14 -0300

    fix: Excesso de renderização e lógíca de requisiçaõ

--- worklab-frontend/src/pages/VincularExame.jsx
+++ worklab-frontend/src/pages/VincularExame.jsx
@@ -1,53 +1,46 @@
 // Importa hooks do React
 import { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form"
 // Importa instância do cliente Axios personalizada
 import axiosClient from "../axios";
 
 // Importa componentes necessários
 import NavBar from "../components/NavBar";
 import ButtonBackMenu from '../components/ButtonBackMenu';
+import ButtonsDefault from '../components/ButtonsDefault';
 
 // Componente principal VincularExame
 const VincularExame = () => {
+  const { register, handleSubmit, reset } = useForm()
   // Estados para controlar dados do formulário e estado de carregamento
-  const [numeroAtendimento, setNumeroAtendimento] = useState('');
   const [exames, setExames] = useState([]);
-  const [exameSelecionado, setExameSelecionado] = useState('');
   const [loading, setLoading] = useState(false);
 
-  // Manipula a mudança no número de atendimento
-  const handleNumeroAtendimentoChange = (e) => {
-    let numeroAtendimento = Number(e.target.value);
-    numeroAtendimento ? setNumeroAtendimento(numeroAtendimento) : setNumeroAtendimento('')
-  };
-
-  // Manipula a seleção de um exame
-  const handleExame = (e) =>  {
-    setExameSelecionado(e.target.value);
-  }
-
    // Manipula o evento de vinculação do exame
-  const handleVincularExame = () => {
+  const handleVincularExame = async (data) => {
     setLoading(true)
 
-    if (numeroAtendimento && exameSelecionado) {
-      // Faz uma requisição POST para vincular exame ao paciente
-      axiosClient.post(`/paciente/${numeroAtendimento}/exame/${exameSelecionado}`)
-        .then(response => {
-          console.log('Exame vinculado com sucesso');
-          setNumeroAtendimento('')
-          setExameSelecionado('');
-          setLoading(false)
-        })
-        .catch(error => {
-          console.error('Erro ao vincular exame:', error);
-          setNumeroAtendimento('')
-          setExameSelecionado('');
-          setLoading(false)
-        });
-    } else {
-      console.log('Número de atendimento do paciente ou exame não informado');
+    try {
+      const { numeroAtendimento, exame } = data;
+
+      if (!numeroAtendimento) {
+        throw new Error('Número do paciente não informado.');
+      }
+
+      if (!exame) {
+        throw new Error('Exame não informado.');
+      }
+
+      const response = await axiosClient.post(`/paciente/${numeroAtendimento}/exame/${exame}`)
+      alert('Exame vinculado com sucesso', response.data)
+
+    } catch (error) {
+      alert(error.message)
+      console.log('Erro ao vincular exame:', error)
     }
+
+    reset()
+    setLoading(false)
   };
 
   // Efeito para buscar exames cadastrados ao carregar o componente
@@ -74,60 +67,50 @@ const VincularExame = () => {
       {/* Renderiza a seção de cabeçalho */}
       <div className="flex flex-col items-center justify-center">
         {/* Renderiza o formulário de vinculação de exame */}
-        <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-6 align-middle'>
-          <div className="sm:col-span-3 flex flex-col">
-            <label htmlFor="numeroAtendimento" className="mb-2">
-              Número de Atendimento:
-            </label>
-            <input
-              type="text"
-              id="numeroAtendimento"
-              name="numeroAtendimento"
-              value={numeroAtendimento}
-              onChange={handleNumeroAtendimentoChange}
-              className="p-2 mb-4  rounded-md ring-1 border-gray-300  text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-400 focus:border-indigo-400 sm:text-sm sm:leading-5 outline-none transition-all duration-300 ease-in-out"
-            />
-          </div>
-            
-          {/* Dropdown para seleção de exame */}
-          <div className="sm:col-span-3">
-            <label htmlFor="exame" className="mb-2">
-              Exame
-            </label>
-            <div className="mt-2">
-              <select
-                id="exame"
-                name="exame"
-                className="block w-full  rounded-md ring-1 border-gray-300 p-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-400 focus:border-indigo-400 sm:text-sm sm:leading-5 outline-none transition-all duration-300 ease-in-out"
-                value={exameSelecionado}
-                onChange={handleExame}
-              >
-                <option value=""></option>
-                {exames.map((exame) => (
-                  <option key={exame.codigo} value={exame.codigo}>
-                    {exame.codigo}
-                  </option>
-                ))}
-              </select>
+        <form onSubmit={handleSubmit(handleVincularExame)}>
+          <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-6 align-middle'>
+            <div className="sm:col-span-3 flex flex-col">
+              <label htmlFor="numeroAtendimento" className="mb-2">
+                Número de Atendimento:
+              </label>
+              <input
+                type="text"
+                id="numeroAtendimento"
+                {...register('numeroAtendimento')}
+                className="p-2 mb-4  rounded-md ring-1 border-gray-300  text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-400 focus:border-indigo-400 sm:text-sm sm:leading-5 outline-none transition-all duration-300 ease-in-out"
+              />
+            </div>
+              
+            {/* Dropdown para seleção de exame */}
+            <div className="sm:col-span-3">
+              <label htmlFor="exame" className="mb-2">
+                Exame
+              </label>
+              <div className="mt-2">
+                <select
+                  id="exame"
+                  name="exame"
+                  className="block w-full  rounded-md ring-1 border-gray-300 p-2 text-gray-900 shadow-sm focus:ring-2 focus:ring-indigo-400 focus:border-indigo-400 sm:text-sm sm:leading-5 outline-none transition-all duration-300 ease-in-out"
+                  {...register('exame')}
+                >
+                  <option value=""></option>
+                  {exames.map((exame) => (
+                    <option key={exame.codigo} value={exame.codigo}>
+                      {exame.codigo}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
           </div>
-        </div>
-        
-      {/* Botões para navegação e vinculação de exame */}         
-       <div className='flex flex-wrap items-center justify-center gap-4 mt-8'>
-        {/* Botão para voltar ao menu */}
-        <ButtonBackMenu />
 
-        {/* Botão para vincular exame */}
-        <button
-          onClick={handleVincularExame}
-          className="rounded-md bg-indigo-500 text-[16px] font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 py-2 px-14"
-        >
-          
-          { loading ? 'Vinculando...' : 'Vincular Exame' }
-        </button>
-       </div>
-       
+          {/* Botões para navegação e vinculação de exame */}         
+          <ButtonsDefault loading={loading} config={{
+            'pLoading': 'Vinculando...',
+            'pTitle' : 'Vincular Exame'
+          }}
+          />
+        </form>
       </div>
     </>
   );
